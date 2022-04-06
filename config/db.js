9db5a0f6f79e74d27e1f33ad75d2c334d359bdcc
hstrejoluna <trejolunatutoriales@gmail.com>
2022-04-05 20:46:06 -0500

    set host

--- config/db.js
+++ config/db.js
@@ -1,12 +1,11 @@
 const Sequelize = require("sequelize");
-require("dotenv").config({ path: ".env.vars" });
+require("dotenv").config({ path: ".env" });
 const db = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
     dialect: "mysql",
     dialectOptions: {},
     define: {
